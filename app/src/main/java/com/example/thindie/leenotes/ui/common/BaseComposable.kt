3ed0eb1ba323b922bf78d8d8dbe3bd67cc39ce9c
Alex <thindie1@mail.ru>
2023-08-14 13:51:31 +0200

    add colors to theme. set base composable fun scheme. set common button

--- app/src/main/java/com/example/thindie/leenotes/ui/common/BaseComposable.kt
+++ app/src/main/java/com/example/thindie/leenotes/ui/common/BaseComposable.kt
@@ -1,20 +1,50 @@
 package com.example.thindie.leenotes.ui.common
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.thindie.leenotes.R
 import com.example.thindie.leenotes.ui.theme.LeenotesTheme
+import com.example.thindie.leenotes.ui.theme.colors
+import com.example.thindie.leenotes.ui.theme.typo
+
 
 @Composable
 fun Qwerty(modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(id = R.string.app_name),
+        style = typo.displayLarge,
+        color = colors.primary
+    )
+
+    Text(
+        text = stringResource(id = R.string.app_name),
+        style = typo.displayMedium,
+        color = colors.secondary
+    )
+
+    Text(
+        text = stringResource(id = R.string.app_name),
+        style = typo.displaySmall,
+        color = colors.tertiary
+    )
 
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun QwertyPreview() {
-    LeenotesTheme {
-       Qwerty()
+    Column {
+        LeenotesTheme {
+            Qwerty()
+        }
+        LeenotesTheme(darkTheme = true) {
+            Qwerty()
+        }
     }
+
 }
--- app/src/main/java/com/example/thindie/leenotes/ui/theme/Type.kt
+++ app/src/main/java/com/example/thindie/leenotes/ui/theme/Type.kt
@@ -8,27 +8,41 @@ import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
+   /* displayLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        letterSpacing = 0.5.sp
+    ),
+    headlineLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 10.sp
     ),
-    labelSmall = TextStyle(
+    titleMedium = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontWeight = FontWeight.Normal,
+        fontSize = 8.sp
+    ),*/
 )
