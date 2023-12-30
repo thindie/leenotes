9be819a87e4d40cfca4bc8f9bc0aa91f4880ee5e
Alex <thindie1@mail.ru>
2023-12-30 14:12:55 +0200

    app: add strings, routing, icons to hub, logging

--- app/src/main/java/com/example/thindie/leenotes/Routing.kt
+++ app/src/main/java/com/example/thindie/leenotes/Routing.kt
@@ -2,10 +2,18 @@ package com.example.thindie.leenotes
 
 import androidx.navigation.NavController
 import com.example.thindie.leenotes.presentation.features.feature_note.concrete_note.concreteNoteRoute
+import com.example.thindie.leenotes.presentation.features.feature_notes.all_notes.routeAllNotes
 
 
 fun NavController.concreteNote(argument: String){
     navigate(route = concreteNoteRoute.plus(argument)){
+        restoreState = true
+        launchSingleTop = true
+    }
+}
+
+fun NavController.allNotes(){
+    navigate(route = routeAllNotes){
         restoreState = true
         launchSingleTop = true
     }
