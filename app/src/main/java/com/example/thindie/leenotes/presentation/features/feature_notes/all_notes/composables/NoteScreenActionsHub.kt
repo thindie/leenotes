f650af193e18012664e03a3e080c23e474ac1e22
Alex <thindie1@mail.ru>
2024-01-03 13:39:36 +0200

    feature_all notes:bottom bar: applied Arrangement space between

--- app/src/main/java/com/example/thindie/leenotes/presentation/features/feature_notes/all_notes/composables/NoteScreenActionsHub.kt
+++ app/src/main/java/com/example/thindie/leenotes/presentation/features/feature_notes/all_notes/composables/NoteScreenActionsHub.kt
@@ -1,14 +1,12 @@
 package com.example.thindie.leenotes.presentation.features.feature_notes.all_notes.composables
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.thindie.leenotes.presentation.features.feature_notes.all_notes.composables.noteshubstate.NoteActionsHubState
@@ -27,6 +25,8 @@ fun NoteActionsHub(
             .fillMaxWidth()
             .padding(horizontal = 20.dp, vertical = 4.dp)
             .wrapContentHeight(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         NoteCreationUnit(notesActionsHubState)
         Actions(notesActionsHubState)
