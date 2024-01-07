aaf899c7a6ea17a94d3ac1f7336ca5b4e68ac91b
Alex <thindie1@mail.ru>
2024-01-07 22:23:22 +0200

    features: routes: fix route naming and dependencies provider getting

--- app/src/main/java/com/example/thindie/leenotes/presentation/features/feature_notes/all_notes/NotesScreenRoute.kt
+++ app/src/main/java/com/example/thindie/leenotes/presentation/features/feature_notes/all_notes/NotesScreenRoute.kt
@@ -1,22 +1,23 @@
 package com.example.thindie.leenotes.presentation.features.feature_notes.all_notes
 
+import android.content.Context
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.example.thindie.leenotes.common.di.App
-import com.example.thindie.leenotes.common.di.DependenciesProvider
-import com.example.thindie.leenotes.presentation.features.feature_notes.viewodel.NotesScreenViewModel
 import com.example.thindie.leenotes.presentation.features.feature_notes.di.NotesScreenComponent
+import com.example.thindie.leenotes.presentation.features.feature_notes.viewodel.NotesScreenViewModel
 
-const val routeAllNotes = "allNotes"
+const val allNotesRoute = "allNotes"
 
 fun NavGraphBuilder.allNotesScreen(onClickMenu: () -> Unit, onClickConcreteNote: (String) -> Unit) {
-    composable(route = routeAllNotes) {
-        val app: App? = LocalContext.current as? App
-        if (app != null){
-            val dependenciesProvider = DependenciesProvider.get(app)
-            val viewModelFactory = provideFactoryFromDaggerComponent(dependenciesProvider)
+    composable(route = allNotesRoute) {
+        val context = LocalContext.current
+        val dependenciesProvider = initDaggerComponent(context = context)
+        if (dependenciesProvider != null) {
+            val viewModelFactory = dependenciesProvider.provideFactory()
             val viewModel: NotesScreenViewModel = viewModel(factory = viewModelFactory)
             NotesScreen(viewModel = viewModel, onSelectArgument = onClickConcreteNote)
         }
@@ -24,10 +25,11 @@ fun NavGraphBuilder.allNotesScreen(onClickMenu: () -> Unit, onClickConcreteNote:
     }
 }
 
-private fun provideFactoryFromDaggerComponent(dependenciesProvider: DependenciesProvider) =
-    NotesScreenComponent.init(dependenciesProvider).provideFactory()
 
-
-
-
-
+@Composable
+private fun initDaggerComponent(context: Context): NotesScreenComponent? {
+    val currentApp = context as? App
+    return if (currentApp != null) {
+        NotesScreenComponent.init(currentApp.getAppComponent())
+    } else null
+}
