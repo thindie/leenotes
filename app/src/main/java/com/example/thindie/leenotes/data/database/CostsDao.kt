5c225799ab61476b3f78ddcda878eac74ff54721
Alex <thindie1@mail.ru>
2024-01-07 22:32:57 +0200

    update costs dao

--- app/src/main/java/com/example/thindie/leenotes/data/database/CostsDao.kt
+++ app/src/main/java/com/example/thindie/leenotes/data/database/CostsDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Query
 import androidx.room.Upsert
 import com.example.thindie.leenotes.data.database.DataBaseContract.costsTable
 import com.example.thindie.leenotes.data.database.entities.CostDbModel
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CostsDao {
@@ -18,6 +19,6 @@ interface CostsDao {
     @Query("DELETE FROM $costsTable WHERE id =:id")
     suspend fun deleteCost(id: Int)
 
-    @Query("SELECT * FROM $costsTable WHERE isBought == :isSpent ORDER  BY price ASC LIMIT :limit")
-    suspend fun getTopCosts(limit: Int, isSpent: Boolean): List<CostDbModel>
+    @Query("SELECT * FROM $costsTable ORDER  BY price ASC  ")
+    fun getTopCosts(): Flow<List<CostDbModel>>
 }
