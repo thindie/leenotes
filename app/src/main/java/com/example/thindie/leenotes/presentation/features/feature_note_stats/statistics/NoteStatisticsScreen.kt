d3fc0b3e8d0ad957dc714f3d578b95d4891cd54b
Alex <thindie1@mail.ru>
2024-01-09 11:53:22 +0200

    feature_statistics note: advanced ui implementation at NotesStatisticsScreen

--- app/src/main/java/com/example/thindie/leenotes/presentation/features/feature_note_stats/statistics/NoteStatisticsScreen.kt
+++ app/src/main/java/com/example/thindie/leenotes/presentation/features/feature_note_stats/statistics/NoteStatisticsScreen.kt
@@ -1,21 +1,35 @@
 package com.example.thindie.leenotes.presentation.features.feature_note_stats.statistics
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.thindie.leenotes.R
+import com.example.thindie.leenotes.common.design_system.IconHolder
+import com.example.thindie.leenotes.common.design_system.getColor
+import com.example.thindie.leenotes.domain.entities.Summary
+import com.example.thindie.leenotes.presentation.IconsHub
+import com.example.thindie.leenotes.presentation.features.feature_note_stats.viewmodel.NotesStatisticsScreenEvent
 import com.example.thindie.leenotes.presentation.features.feature_note_stats.viewmodel.NotesStatisticsViewModel
 
 @Composable
@@ -25,21 +39,53 @@ fun NotesStatisticsScreen(modifier: Modifier = Modifier, viewModel: NotesStatist
     Column(
         modifier = modifier
             .padding(horizontal = 12.dp)
-            .fillMaxSize()
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         TotalNotes(int = state.summary?.totalNotes)
         TotalSpent(int = state.summary?.totalSpent)
         TotalTempAndPlanned(
-            temp = state.summary?.totalTempNotes,
-            plannedSpent = state.summary?.totalPlannedToSpentNotes,
-            plannedCosts = state.summary?.totalPlannedCosts
+            temp = state.summary?.tempNotes,
+            plannedSpent = state.summary?.spentNotes,
+            plannedCosts = state.summary?.plannedCosts
         )
         TopSpentAndTopPlanned(
             topSpent = state.summary?.topSpent,
             topPlanned = state.summary?.topPlanned
         )
+        Spacer(modifier = modifier.weight(1f, true))
+        ControlRow(onEvent = viewModel::onEvent, state.summary)
     }
 
+}
+
+@Composable
+fun ControlRow(onEvent: (NotesStatisticsScreenEvent) -> Unit, summary: Summary?) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        OutlinedIconButton(onClick = { onEvent(NotesStatisticsScreenEvent.BackWard) }) {
+            Icon(
+                painter = IconHolder.render(IconsHub.left).getIcon(),
+                contentDescription = null
+            )
+        }
+
+        OutlinedButton(onClick = { onEvent(NotesStatisticsScreenEvent.Year) }) {
+            Text(text = stringResource(R.string.text_field_all))
+        }
+
+        OutlinedIconButton(onClick = { onEvent(NotesStatisticsScreenEvent.Forward) }) {
+            Icon(
+                painter = IconHolder.render(IconsHub.right).getIcon(),
+                contentDescription = null
+            )
+        }
+    }
 }
 
 @Composable
@@ -48,13 +94,35 @@ fun TopSpentAndTopPlanned(
     topSpent: List<String>?,
     topPlanned: List<String>?,
 ) {
-    Row(modifier = modifier.fillMaxWidth()) {
-        LazyColumn(modifier = modifier.fillMaxWidth(0.5f)) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        LazyColumn(
+            modifier = modifier
+                .background(getColor())
+                .fillMaxWidth(0.5f)
+        ) {
+            item {
+                Text(
+                    modifier = modifier.padding(bottom = 20.dp),
+                    text = stringResource(id = R.string.text_label_top_from_spent),
+                    style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold)
+                )
+            }
             items(topSpent.orEmpty()) {
                 Text(text = it)
             }
         }
-        LazyColumn(modifier = modifier.fillMaxWidth(0.5f)) {
+        LazyColumn(
+            modifier = modifier
+                .background(getColor())
+                .fillMaxWidth(0.5f)
+        ) {
+            item {
+                Text(
+                    modifier = modifier.padding(bottom = 20.dp),
+                    text = stringResource(id = R.string.text_label_top_from_planned),
+                    style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold)
+                )
+            }
             items(topPlanned.orEmpty()) {
                 Text(text = it)
             }
@@ -65,16 +133,22 @@ fun TopSpentAndTopPlanned(
 @Composable
 private fun TotalNotes(modifier: Modifier = Modifier, int: Int?) {
     if (int != null)
-        Row(modifier = modifier) {
-            Text(text = stringResource(id = R.string.text_label_total_notes, int))
+        Row(modifier = modifier.background(getColor())) {
+            Text(
+                text = stringResource(id = R.string.text_label_total_notes, int),
+                style = MaterialTheme.typography.titleLarge
+            )
         }
 }
 
 @Composable
 private fun TotalSpent(modifier: Modifier = Modifier, int: Double?) {
     if (int != null)
-        Row(modifier = modifier) {
-            Text(text = stringResource(id = R.string.text_label_total_spent, int))
+        Row(modifier = modifier.background(getColor())) {
+            Text(
+                text = stringResource(id = R.string.text_label_total_spent, int),
+                style = MaterialTheme.typography.titleLarge
+            )
         }
 }
 
@@ -86,12 +160,27 @@ private fun TotalTempAndPlanned(
     plannedCosts: Double?,
 ) {
 
-    Row(modifier = modifier) {
+    Row(
+        modifier = modifier
+            .background(getColor())
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
         if (temp != null)
-            Text(text = stringResource(id = R.string.text_label_total_temp_notes, temp))
+            Text(
+                text = stringResource(id = R.string.text_label_total_temp_notes, temp),
+                style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold)
+            )
         if (plannedSpent != null)
-            Text(text = stringResource(id = R.string.text_label_planned_to_spent, plannedSpent))
+            Text(
+                text = stringResource(id = R.string.text_label_planned_to_spent, plannedSpent),
+                style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Bold)
+            )
         if (plannedCosts != null)
-            Text(text = stringResource(id = R.string.text_label_planned_costs, plannedCosts))
+            Text(
+                text = stringResource(id = R.string.text_label_planned_costs, plannedCosts),
+                style = MaterialTheme.typography.titleLarge
+            )
     }
 }
