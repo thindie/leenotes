1fcf9cfc16a02d88d236ee6a015e28a3d069a193
Alex <thindie1@mail.ru>
2024-01-10 22:49:16 +0200

    app: add app icons ^^, change app name

--- app/src/main/java/com/example/thindie/leenotes/presentation/IconsHub.kt
+++ app/src/main/java/com/example/thindie/leenotes/presentation/IconsHub.kt
@@ -1,30 +1,25 @@
 package com.example.thindie.leenotes.presentation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Done
-import androidx.compose.material.icons.outlined.Edit
 import com.example.thindie.leenotes.R
 
 object IconsHub {
-     val left = Icons.Default.KeyboardArrowLeft
+    val left = Icons.Default.KeyboardArrowLeft
     val right = Icons.Default.KeyboardArrowRight
-     val web = R.drawable.icon_web
+    val web = R.drawable.icon_web
     val confirm = Icons.Default.Check
     val create = Icons.Default.Create
     val cancel = Icons.Default.Close
     val send = Icons.Default.Send
     val save = Icons.Outlined.Done
     val delete = Icons.Outlined.Delete
+    val wrabbyte = R.drawable.wrabbyte
 }
